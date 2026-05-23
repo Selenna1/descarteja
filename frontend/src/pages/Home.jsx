f912ce3d5e2f8a996c24922744e5512483e15515
Selenna1 <selenna1313@gmail.com>
2026-05-23 09:36:39 -0300

    Melhorando conteúdo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,7 +23,18 @@ function Home() {
           <h2 className="title">Objetivo do sistema</h2>
           <p>Facilitar o acesso da população a informação sobre reciclagem eletrônica e incentivar práticas sustentáveis através de uma plataforma digital simples e acessível.</p>
         </section>
-      </main>
+        <section className="card">
+         <h2 className="title">O que é lixo eletrônico?</h2> 
+         <p> O lixo eletrônico é composto por equipamentos eletrônicos descartados, como computadores, celulares, baterias, televisores e cabos. Quando descartados incorretamente, esses materiais podem causar sérios impactos ambientais.</p>
+        </section>
+        <section className="card">
+          <h2 className="title">Impactos ambientais</h2>
+          <p>Componentes eletrônicos possuem substâncias tóxicas que podem cotaminar o solo, rios e lençóis freáticos, afetando o meio ambiente e a saúde da população.</p>
+        </section>
+        <section className="card">
+          <h2 className="title">Sustentabilidade e conscientização</h2>
+          <p>O DescarteJá busca incentivar práticas sustentáveis por meio da conscientizzação ambiental e da facilitação do acesso a pontos de coleta de resíduos eletrônicos. </p>
+        </section>
       <Footer />
     </div>
   )
