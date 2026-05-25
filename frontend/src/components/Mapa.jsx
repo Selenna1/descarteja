ca7a36760317e11b51c0555059fa299b91d78088
Selenna1 <selenna1313@gmail.com>
2026-05-25 09:38:55 -0300

    Corrigindo logo do mapa

--- frontend/src/components/Mapa.jsx
+++ frontend/src/components/Mapa.jsx
@@ -1,10 +1,14 @@
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup
-} from "react-leaflet"
-import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const iconeReciclagem = new L.Icon({
+  iconUrl: "/marker-reciclagem.png",
+  iconSize: [45, 45],
+  iconAnchor: [22, 45],
+  popupAnchor: [0, -40],
+});
+
 function Mapa({ pontos }) {
   return (
     <MapContainer
@@ -14,36 +18,29 @@ function Mapa({ pontos }) {
         height: "500px",
         width: "100%",
         borderRadius: "12px",
-        marginTop: "30px"
+        marginTop: "30px",
       }}
     >
       <TileLayer
-        attribution='&copy; OpenStreetMap'
+        attribution="&copy; OpenStreetMap"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {pontos
-        .filter(
-          (ponto) =>
-            ponto.latitude && ponto.longitude
-        )
+        .filter((ponto) => ponto.latitude && ponto.longitude)
         .map((ponto, index) => (
           <Marker
             key={index}
-            position={[
-              ponto.latitude,
-              ponto.longitude
-            ]}
+            position={[ponto.latitude, ponto.longitude]}
+            icon={iconeReciclagem}
           >
             <Popup>
-              <strong>
-                {ponto.nome}
-              </strong>
+              <strong>{ponto.nome}</strong>
               <br />
               {ponto.endereco}
             </Popup>
           </Marker>
         ))}
     </MapContainer>
-  )
+  );
 }
-export default Mapa
+export default Mapa;
